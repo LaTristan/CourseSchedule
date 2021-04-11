28110efde3aa5ad74b34486061f58745aab10737
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-04-11 23:55:14 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   Course.h 	new file:   runtime.cpp

---
+++ Course.h
@@ -0,0 +1,15 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+class Course{
+
+};
+
+#endif
