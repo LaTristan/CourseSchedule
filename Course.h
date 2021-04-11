69c5e54a8f6b4d2c1d40984913d00b208276d8c0
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-04-12 00:11:49 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Course.h 	new file:   Table.h

--- Course.h
+++ Course.h
@@ -7,9 +7,41 @@
 #include <algorithm>
 #include <functional>
 using namespace std;
+enum Day {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+};
+enum Distribution{
+    First,
+    Second,
+    Third,
+    Fourth,
+    Fifth,
+    Sixth
+};
 
 class Course{
-
+private:
+    string name;
+    vector<int>weeks;
+    vector<pair<Day,Distribution>> day_in_week;
+    string teacher;
+    float points;
+public:
+    bool searchByName(string nameStr){
+        if(name.find(nameStr)!=string::npos)return true;
+        return false;
+    }
+    bool searchByTeacher(string tName){
+        if(teacher.find(tName)!=string::npos)return true;
+        return false;
+    }
+    Course(string name,string teacher,float pts):name(name),teacher(teacher),points(pts){}
 };
 
 #endif
---
+++ Table.h
@@ -0,0 +1,10 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "Course.h"
+
+class Table{
+private:
+    vector<Course> table;
+};
+#endif
